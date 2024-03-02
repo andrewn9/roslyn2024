35f6a65a5efc11bceb90ea7cebd500d8e53ce93e
Lance Phung <lncphung@gmail.com>
2024-03-02 09:47:44 -0500

    Added Matter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello world!");
+import { Engine, Render, Runner, Bodies, Composite } from 'matter-js'
+
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+
+const engine = Engine.create();
+
+const render = Render.create({
+    engine: engine,
+    canvas: canvas
+});
+
+const player = Bodies.rectangle(400, 200, 10, 10);
+
+const map = [Bodies.rectangle(400, 600, 100, 10, { isStatic: true })];
+
+Composite.add(engine.world, [...map, player]);
+
+Render.run(render);
+
+const runner = Runner.create();
+Runner.run(runner, engine);
